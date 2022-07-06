3c486ce72b18cde5799594d7c703dde0181d34c6
Felix Negoita <felixnegoita@Felixs-MacBook-Pro.local>
2022-07-06 20:35:06 +0300

    parsed csv lines

--- planets-io/index.js
+++ planets-io/index.js
@@ -1,2 +1,26 @@
 import { parse } from "csv-parse";
+import fs from "fs";
 
+
+const results = [];
+
+/**
+ * @param {{koi_disposition:string}} planet
+ */
+function isHabitablePlanet(planet) {
+    return planet.koi_disposition === 'CONFIRMED';
+}
+
+fs.createReadStream('kepler_data.csv')
+    .pipe(parse({
+        comment: '#',
+        columns: true,
+    }))  //returns a stream as a result
+    .on('data', (data) => {
+        results.push(data);
+    })
+    .on('error', (error) => { console.log(error) })
+    .on('end', () => {
+        console.log('Completed reading file. Contents below:');
+        console.log(results);
+    })
