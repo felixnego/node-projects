40ccd2c232767e473c24845e211a62a330aba802
Felix Negoita <felixnegoita@Felixs-MacBook-Pro.local>
2022-07-06 20:47:32 +0300

    filtered planets

--- planets-io/index.js
+++ planets-io/index.js
@@ -2,13 +2,21 @@ import { parse } from "csv-parse";
 import fs from "fs";
 
 
-const results = [];
+const habitablePlanets = [];
 
 /**
- * @param {{koi_disposition:string}} planet
+ * @param {
+ * {
+ * koi_disposition:string,
+ * koi_insol:string,
+ * koi_prad: string
+ * }
+ * } planet
  */
 function isHabitablePlanet(planet) {
-    return planet.koi_disposition === 'CONFIRMED';
+    return planet.koi_disposition === 'CONFIRMED'
+        && planet.koi_insol > 0.36 && planet.koi_insol < 1.11
+        && planet.koi_prad < 1.6;
 }
 
 fs.createReadStream('kepler_data.csv')
@@ -17,10 +25,16 @@ fs.createReadStream('kepler_data.csv')
         columns: true,
     }))  //returns a stream as a result
     .on('data', (data) => {
-        results.push(data);
+        if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+        }
     })
     .on('error', (error) => { console.log(error) })
     .on('end', () => {
-        console.log('Completed reading file. Contents below:');
-        console.log(results);
+        console.log('Completed reading file. Results below:');
+        console.log(`${habitablePlanets.length} habitable planets found!`);
+        console.log(habitablePlanets.map((planet) => {
+            /** @namespace planet.kepler_name **/
+            return planet.kepler_name;
+        }))
     })
