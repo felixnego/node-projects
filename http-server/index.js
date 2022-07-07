8650d44bf91cb52fa914622875f93b2ce97643f3
Felix Negoita <felixnegoita@192-168-0-196.rdsnet.ro>
2022-07-07 17:45:00 +0300

    added to http server

--- http-server/index.js
+++ http-server/index.js
@@ -4,13 +4,27 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'application/json'
-    });
-    res.end(JSON.stringify({
-        id: 1,
-        name: 'Sir Issac Newton'
-    }));
+    if (req.url === '/friends') {
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        res.end(JSON.stringify({
+            id: 1,
+            name: 'Sir Issac Newton'
+        }));
+    } else if (req.url === '/message') {
+        res.write('<html lang="">');
+        res.write('<body>');
+        res.write('<ul>');
+        res.write('<li>Hello There</li>');
+        res.write('</ul>');
+        res.write('<body>');
+        res.write('</html>');
+        res.end();
+    } else {
+        res.statusCode = 404;
+        res.end()
+    }
 });
 
 server.listen(PORT, () => {
