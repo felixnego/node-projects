8b108a65f5dfc5725ccf0787970692b33e697591
Felix Negoita <felixnegoita@Felixs-MacBook-Pro.local>
2022-08-20 14:57:57 +0300

    added security example

--- security/server.js
+++ security/server.js
@@ -5,6 +5,8 @@ const express = require('express');
 const helmet = require('helmet');
 const passport = require('passport');
 const { Strategy } = require('passport-google-oauth20');
+const cookieSession = require('cookie-session');
+const { verify } = require('crypto');
 require('dotenv').config();
 
 
@@ -26,11 +28,28 @@ passport.use(new Strategy({
     clientSecret: config.CLIENT_SECRET
 }, verifyCallback));
 
-app.use(helmet());
-app.use(passport.initialize());
+// Save the session to the cookie
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
 
-function checkLoggedIn(req, res, next) {
-    const isLoggedIn = true; // TODO
+// Read the session from the cookie
+passport.deserializeUser((obj, done) => {
+    done(null, obj);
+})
+
+app.use(helmet());
+app.use(cookieSession({
+    name: 'session',
+    maxAge: 60 * 60 * 1000,
+    keys: ['secret key']
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+function checkLoggedIn(req, res, next) {  // req.user
+    console.log('Current user is', req.user);
+    const isLoggedIn = req.isAuthenticated() && req.user;
     if (!isLoggedIn) {
         return res.status(401).json({
             error: 'You must log in to access this resource!'
@@ -50,12 +69,15 @@ app.get('/auth/google', passport.authenticate('google', {
 app.get('/auth/google/callback', passport.authenticate('google', {
     failureRedirect: '/failure',
     successRedirect: '/',
-    session: false
+    session: true
 }), (req, res) => {
     console.log('Google called us back.');
 });
 
-app.get('/auth/logout', (req, res) => {});
+app.get('/auth/logout', (req, res) => {
+    req.logout();  // clears session and removes req.user
+    return res.redirect('/');
+});
 
 app.get('/secret', checkLoggedIn, (req, res) => {
     return res.send('Your personal Secret Value is 42!');
